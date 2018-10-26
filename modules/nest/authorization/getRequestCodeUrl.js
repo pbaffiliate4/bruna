f18eb6170bb3f2af2e02be34a0ced7605ff64151
Afaf Zakhya <pbaffiliate4@gmail.com>
2018-10-26 15:08:59 +0300

    Update getRequestCodeUrl.js

--- modules/nest/authorization/getRequestCodeUrl.js
+++ modules/nest/authorization/getRequestCodeUrl.js
@@ -1,19 +1,27 @@
  
  
- /**
- * this script is the first step of the oauth 2 process
- * it returns a url to invoke from a web client to open nest authentication window
- * where the end user has to enter his crendentials and authorize the scriptr; application
- * @module getRequestCodeUrl
- */
+ var tokenManager = require("./TokenManager.js");
 
-var config = require("../config.js");
-var util = require("../util.js");
+try {
+  
+  var params = request.parameters;
 
-var urlConfig = config.getNestAuthUrl();
-var username = request.parameters.username;
-var state = urlConfig.state;
-if (username) {
-  storage.global[state] = username;
-}
-return urlConfig.url + "&state=" + state;			
+  // check for errors
+  if (params.error) {
+
+    return {
+      "status": "failed",
+      "errorCode": "Authentication_Failed",
+      "errorDetail": "Received: " +  params.error + " - " + params.error_description 
+    }
+  }
+
+  return tokenManager.getAccessToken(params); 
+}catch(exception) {
+  
+  return {
+    "status": "failure",
+    "errorCode": exception.errorCode ? exception.errorCode : "Internal_Error",
+    "errorDetail": exception.errorDetail ? exception.errorDetail : exception
+  };
+}			
