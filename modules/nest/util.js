63e3d726e670dd096e08d9366c546fff08cab200
Afaf Zakhya <pbaffiliate4@gmail.com>
2018-10-26 10:13:24 +0300

    Update util.js

--- modules/nest/util.js
+++ modules/nest/util.js
@@ -3,13 +3,13 @@
   /*
  * Turns dots in the username into "_dot_" to avoid problems when persisting the username as a property key
  */
-toStorableUserName = function(username) {
+toStorableUserName = function(usernamem) {
   return username.replace(/\./g, "_dot_");
 };
 
 /*
  * Turns "_dot_" in the username into "." 
  */
-fromStorableUserName = function(username) {
+fromStorableUserName = function(usernamem) {
   return username.replace(/_dot_/g, ".");
 };					
